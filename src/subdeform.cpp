690f5f4058dd5aa7acba56f7df436d0342a84699
symek <szymon.kapeniak@gmail.com>
2018-02-18 20:03:50 +0100

    Adding option to save matrix as a pose space deformation.

--- src/subdeform.cpp
+++ src/subdeform.cpp
@@ -11,6 +11,61 @@
 
 namespace po = hboost::program_options;
 
+bool create_shape_matrix(const std::string & restfile, const StringVec &skinfiles,
+    const StringVec &shapefiles, Matrix &matrix)
+{
+    GU_Detail rest;
+    if(!rest.load(restfile.c_str()).success()) {
+        std::cerr << "Can't open rest file " << restfile << '\n';
+        return false;
+    }
+
+    const int npoints = rest.getNumPoints();
+    matrix.conservativeResize(npoints*3, shapefiles.size());
+    GA_Offset ptoff;
+    int shapenum = 0;
+    GU_Detail shape;
+    GU_Detail skin;
+    StringVec::const_iterator it;
+    for (it=shapefiles.begin(); it!= shapefiles.end(); ++it, ++shapenum) {
+        auto & file = *it;
+        if(!shape.load(file.c_str()).success()) {
+            std::cerr << "Can't open shape file, ignoring it: " << file << '\n';
+            // shapenum++;
+            continue;
+        }
+        auto & skinfile = skinfiles.at(shapenum); 
+        if(!skin.load(skinfile.c_str()).success()) {
+            std::cerr << "Can't open skin file, ignoring it: " << skinfile << '\n';
+            // shapenum++;
+            continue;
+        }
+
+        if(npoints != shape.getNumPoints() || npoints != skin.getNumPoints()) {
+            std::cerr << "Point count doesn't match, ignoring these files: " << file << ": " << skinfile<< '\n';
+            // shapenum++;
+            continue;
+        }
+
+        GA_FOR_ALL_PTOFF(&rest, ptoff) {
+            const GA_Index   rest_index = rest.pointIndex(ptoff);
+            // const UT_Vector3 rest_pos   = rest.getPos3(ptoff);
+            const GA_Offset  skin_off   = skin.pointOffset(rest_index);
+            const UT_Vector3 skin_pos   = skin.getPos3(skin_off);
+            const GA_Offset  shape_off  = shape.pointOffset(rest_index);
+            const UT_Vector3 shape_pos  = shape.getPos3(shape_off);
+            const UT_Vector3 shape_delta(shape_pos - skin_pos);
+            matrix(3*rest_index+0, shapenum) = shape_delta.x();
+            matrix(3*rest_index+1, shapenum) = shape_delta.y();
+            matrix(3*rest_index+2, shapenum) = shape_delta.z(); 
+        }
+        // shapenum++;
+    }
+
+    return true;
+}
+
+
 bool create_shape_matrix(const std::string &restfile, 
     const StringVec &shapefiles, Matrix &matrix)
 {
@@ -74,10 +129,16 @@ int main(int argc, char *argv[])
         auto & shapefiles = result["shapes"].as<StringVec>();
         auto & restfile   = result["rest"].as<std::string>();
         StringVec skinfiles;
+
         if(result.count("skin")) {
             skinfiles = result["skin"].as<StringVec>();
             std::cout << "Using " << shapefiles.size() <<  " skins: " \
             << skinfiles[0] << "...\n";
+        }
+
+        if (skinfiles.size() == 0 && shapefiles.size() != 0) {
+            std::cerr << "Shapes and skin files don't match." << '\n';
+            return 1;
         }
 
         std::cout << "Using rest file: " <<  restfile << '\n';
@@ -85,9 +146,16 @@ int main(int argc, char *argv[])
             << shapefiles[0] << "...\n";
 
         Matrix shapes_matrix;
-        if (!create_shape_matrix(restfile, shapefiles, shapes_matrix)) {
-            std::cerr << "Can't create shape matrix." << '\n';
-            return 1;   
+        if (skinfiles.size() != 0) {
+            if (!create_shape_matrix(restfile, skinfiles, shapefiles, shapes_matrix)) {
+                std::cerr << "Can't create shape matrix." << '\n';
+                return 1;  
+            }
+        } else {
+            if (!create_shape_matrix(restfile, shapefiles, shapes_matrix)) {
+                std::cerr << "Can't create shape matrix." << '\n';
+                return 1;   
+            }
         }
 
         const std::string matrix_file("../tmp/shapes.matrix");
