f39deae9eff123ea4b5ea94688c0afddd60e70ff
symek <szymon.kapeniak@gmail.com>
2018-02-19 01:02:39 +0100

    Adding orthogonalization of PCA matrix.

--- src/utils.hpp
+++ src/utils.hpp
@@ -4,6 +4,70 @@ using StringVec = std::vector<std::string>;
 using Matrix    = Eigen::MatrixXd;
 using Vector    = Eigen::VectorXd;
 
+// Should we just use EIGEN::QRMatrix?
+void orthogonalize_matrix(Matrix & matrix, int c=0) {
+    if(c >= matrix.cols())
+      return;
+    for (int x = c; x < matrix.cols(); ++x) {
+        // project out other components
+        for (int y = 0; y < x; ++y) {
+            double dot = matrix.col(y).dot(matrix.col(x));
+            matrix.col(x) -= dot * matrix.col(y);
+        }
+        double norm2 = matrix.col(x).norm();
+        if(norm2 >= 1e-6) {
+            matrix.col(x) *= 1.0 / norm2;
+        }
+    }
+}
+
+
+bool computePCA(Matrix & matrix, Matrix & pcamatrix, 
+    double variance, bool shift=false, bool orthogonalize=false) {
+
+    Vector eigenvalues;
+    const int rows = matrix.rows();
+    const int cols = matrix.cols();
+
+    if(shift) {
+        for(int x = 0; x < rows; x++) {
+            double mean = matrix.row(x).sum() / (double)cols;
+            matrix.row(x) = (matrix.row(x).array() - mean).matrix();
+        }
+        matrix *= 1.0 / sqrt(cols - 1);
+    }
+
+    Eigen::JacobiSVD<Matrix> eigenSystem(matrix, Eigen::ComputeThinU);
+    const Vector & singularValues = eigenSystem.singularValues();
+    pcamatrix = eigenSystem.matrixU();
+    eigenvalues.resize(pcamatrix.cols());
+    for(int x = 0; x < pcamatrix.cols(); x++) {
+        eigenvalues[x] = singularValues[x] * singularValues[x];
+    }
+
+    double sum_   = eigenvalues.sum();
+    double cutoff = variance * sum_;
+    double keep_ = 0;
+    int pcarank  = 0;
+
+    for(int i = 0; i < eigenvalues.size(); ++i) {
+        keep_ += eigenvalues[i];
+        pcarank++;
+        if(keep_ >= cutoff)
+            break;
+    }
+
+    if(pcarank < pcamatrix.cols()) {
+        pcamatrix.conservativeResize(pcamatrix.rows(), pcarank);
+    }
+
+    if (orthogonalize)
+        orthogonalize_matrix(pcamatrix);
+
+    return true;
+}
+
+
 bool write_matrix(const Matrix & matrix, const char * filename) {
     const double * data = matrix.data();
     // Eigen::Map<Matrix>(data, matrix.rows(), matrix.cols()) = matrix;
