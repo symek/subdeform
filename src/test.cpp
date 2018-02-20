11e44ae311e424b2444ee46a93cf5aff9c7a620a
symek <szymon.kapeniak@gmail.com>
2018-02-20 13:02:37 +0100

    Minor.

--- src/test.cpp
+++ src/test.cpp
@@ -13,11 +13,21 @@ int main()
     for (int i=0; i< 3*3; ++i)
         data[i] = i;
 
-    const Eigen::VectorXd & d = m.row(0);
-    UT_Vector3 v(d.data());
+    // const Eigen::VectorXd & d = m.row(0);
+    // UT_Vector3 v(d.data());
     std::cout << m << '\n';
+    // std::cout << d << '\n';
+    // std::cout << v.x() << v.y() << v.z() << '\n';
+
+
+    Eigen::VectorXd d(3);
+    d.segment<3>(0) = m.row(1);
     std::cout << d << '\n';
-    std::cout << v.x() << v.y() << v.z() << '\n';
+
+    Eigen::VectorXd r = d.transpose() * m;
+    std::cout << r << '\n';    
+
+
     return 0;
 
 }
